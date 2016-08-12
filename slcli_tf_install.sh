84ac6fb4f82477611d49988cf0c4cbb3ac3260a4
hemacb <bhc1230@gmail.com>
2016-08-12 17:43:57 +0530

    SH Script updated
    SH Script updated

--- slcli_tf_install.sh
+++ slcli_tf_install.sh
@@ -1,31 +1,33 @@
-#!/bin/bash
+#!/bin/bash -e
+##SLCLI and Terraform Installation
 
-uname -a > osname.txt
-cat osname.txt | grep 'Ubuntu'
+uname -a | grep 'Ubuntu' > /dev/null
 osname=`echo $?`
-rm -f osname.txt
+
+echo "Now we will install Softlayer CLI. Please sit back and Relax!"
+echo
+echo
 slcli --version &> /dev/null
 check1=`echo $?`
-if [ $check1 == 0 ]
+if [ $check1 -eq 0 ]
         then
         echo "Softlayer CLI is already installed!"
 else
         echo "Installing Softlayer CLI. Please sit back and Relax..!!!"
-		echo
-		echo
         curl -OL https://github.com/softlayer/softlayer-python/zipball/master
         unzip master
         con1=`echo $?`
-        if [ $con1 == 0 ]
-                then                
-                cd softlayer-softlayer-python-4932cac
+        if [ $con1 -eq 0 ]
+                then
+                file=`ls | grep 'softlayer'`
+                cd $file
                         python setup.py install
                         con2=`echo $?`
-                        if [ $con2 == 0 ]
+                        if [ $con2 -eq 0 ]
                         then
                                 echo "Installation Complete"
 						else	
-							if [ $osname == 0 ]
+							if [ $osname -eq 0 ]
 							then 
 								apt-get install python-setuptools -y
                                 python setup.py install
@@ -35,7 +37,7 @@ else
 							fi
 						fi
 		else
-			if [ $osname == 0 ]
+			if [ $osname -eq 0 ]
 			then 
 				apt-get update -y
                 apt-get install unzip -y
@@ -47,11 +49,11 @@ else
 			cd softlayer-softlayer-python-4932cac
 				python setup.py install
 				con3=`echo $?`
-				if [ $con3 == 0 ]
+				if [ $con3 -eq 0 ]
 				then
 						echo "Installation Complete"
 				else
-					if [ $osname == 0 ]
+					if [ $osname -eq 0 ]
 					then
 						apt-get install python-setuptools -y
                         python setup.py install
@@ -66,9 +68,12 @@ echo "Version details are as follows:"
 slcli --version
 echo
 echo
+echo "Now we will install Terraform. Please sit back and Relax!"
+echo
+echo
 terraform --version &> /dev/null
 check1=`echo $?`
-if [ $check1 == 0 ]
+if [ $check1 -eq 0 ]
         then
         echo "Terraform is already installed!"
 else
@@ -78,7 +83,7 @@ else
         cd terraform_dir
         wget https://releases.hashicorp.com/terraform/0.7.0/terraform_0.7.0_linux_amd64.zip
         unzip terraform_0.7.0_linux_amd64.zip
-    if [ $osname == 0 ]
+    if [ $osname -eq 0 ]
 	then
 		echo "export PATH=$PATH:/root/terraform_dir" >> /root/.bashrc
 		source /root/.bashrc
@@ -86,7 +91,9 @@ else
 		echo "export PATH=$PATH:/root/terraform_dir" >> /root/.bash_profile
 		source /root/.bash_profile		
     fi
-	echo "Terraform installation is complete!"
 fi
+echo "Installation is complete!"
 echo "Version details are as follows:"
 terraform --version
+
+#EOS
